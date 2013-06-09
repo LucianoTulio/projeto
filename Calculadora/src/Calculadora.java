9796099005f8758c5ca433c674afa5700dd60f38
LucianoTulio <luciano.tulio@me.com>
2013-06-09 10:08:29 -0300

    inicialização

---
+++ Calculadora/src/Calculadora.java
@@ -0,0 +1,31 @@
+
+public class Calculadora {
+
+	
+	public int somar(int x , int y){
+		return x+y;
+	}
+	
+	public int subtrair(int x,int y){
+		return x-y;
+	}
+	
+	public int multiplicar(int x, int y){
+		return x*y;
+	}
+	
+	public int dividir (int x, int y){
+		return  x/y;
+	}
+	
+	public int tempodeexecussao (){
+		int contador=0; 
+		while(contador<1000000000){
+			contador++;
+		}
+		
+		return  contador;
+	}
+
+}
+
